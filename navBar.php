3bd7097896e37d2ceb0887b7889ca0f7a7d7a70d
Julien LATHUILLERE <julien.lathuillere17@gmail.com>
2021-03-20 21:07:40 +0100

    dev

--- navBar.php
+++ navBar.php
@@ -7,4 +7,8 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
+    <a class="navbar-brand navbar-login" href="../index.php">Login</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 </nav>
