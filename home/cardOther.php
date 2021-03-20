035854666c7556d998f804990903a3f4563f2629
Julien LATHUILLERE <jlathuillere@dedicast.fr>
2021-03-20 19:28:27 +0100

    #HOTFIX : too much branch

---
+++ home/cardOther.php
@@ -0,0 +1,9 @@
+<div class="card" style="width: 18rem;">
+    <div class="card-header">
+        <h5 class="card-title">Autres infos</h5>
+    </div>
+    <div class="card-body">
+        <p class="card-text"><?php echo "Autres infos"; ?></p>
+        <button type="button" class="btn btn-primary">Plus d'infos</button>
+    </div>
+</div>
