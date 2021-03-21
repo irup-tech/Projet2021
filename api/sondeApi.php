e0a6c63b268778fbd270adb1f2999be3248aeb1f
Julien LATHUILLERE <julien.lathuillere17@gmail.com>
2021-03-21 11:16:53 +0100

    update dev conformly

---
+++ api/sondeApi.php
@@ -0,0 +1,36 @@
+<?php
+
+$sql = "INSERT INTO data(uid,num,valeur) VALUES (:uuid, :num, :valeur)";
+$getuuid = 'SELECT uuid FROM sondes WHERE uuid = :uuid';
+
+if(isset($_POST['uuid']) && isset($_POST['num']) && isset($_POST['valeur'])){
+
+    require '../database/connect_to_db.php';
+
+    print $_POST ['uuid'];
+    print $_POST ['num'];
+    print $_POST ['valeur'];
+
+    $uuid = $_POST ['uuid'];
+    $sth = $pdo->prepare($getuuid);
+    $sth->execute(array(':uuid' => $uuid));
+    $data = $sth->fetch();
+    if ($data['uuid'] == $uuid) {
+
+        $sth = $pdo->prepare($sql);
+        $sth->execute(array(
+            ":uuid" => $_POST['uuid'],
+            ":num" => $_POST['num'],
+            ":valeur" => $_POST['valeur'],
+        ));
+
+    } else {
+        print 'Unknow';
+    }
+
+} else {
+    print 'Error';
+};
+
+
+?>
--- navBar.php
+++ navBar.php
@@ -7,7 +7,7 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand navbar-login" href="../index.php">Login</a>
+    <a class="navbar-brand navbar-login" href="users/login.php">Login</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
